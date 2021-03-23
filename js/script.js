34425e73ce27729a5429cb018e3372eb6be54745
tophoto <80478062+tophoto@users.noreply.github.com>
2021-03-23 22:07:52 +0300

    1

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function menuToggle(){
-   const toggleMenu = document.querySelector('.toggle');
-   const section = document.querySelector('section');
-   toggleMenu.classList.toggle('active');
-   section.classList.toggle('active');
-}
