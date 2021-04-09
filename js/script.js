a7753969c14f96249c54ddb15e5373fd412eb36e
tophoto <80478062+tophoto@users.noreply.github.com>
2021-04-09 12:57:37 +0300

    5

--- js/script.js
+++ js/script.js
@@ -4,17 +4,37 @@
 //    navigation.classList.toggle('active')
 // }
 
+// var swiper = new Swiper('.swiper-container', {
+//    effect: 'coverflow',
+//    grabCursor: true,
+//    centeredSlides: true,
+//    slidesPerView: 'auto',
+//    coverflowEffect: {
+//    rotate: 0,
+//    stretch: 0,
+//    depth: 150,
+//    modifier: 3,
+//    slideShadows: true,
+//    },
+//    loop: true,
+// });
+
+// var swiper = new Swiper('.swiper-container', {
+//    direction: 'vertical',
+//    pagination: {
+//      el: '.swiper-pagination',
+//      clickable: true,
+//    },
+//  });
+
 var swiper = new Swiper('.swiper-container', {
-   effect: 'coverflow',
+   effect: 'flip',
    grabCursor: true,
-   centeredSlides: true,
-   slidesPerView: 'auto',
-   coverflowEffect: {
-   rotate: 0,
-   stretch: 0,
-   depth: 150,
-   modifier: 3,
-   slideShadows: true,
+   pagination: {
+     el: '.swiper-pagination',
    },
-   loop: true,
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
 });
